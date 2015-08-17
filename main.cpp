daf201190095fd698ddaca4472f41515737450ea
helene <helene@Foch.home>
2015-08-18 00:45:20 +0200

    Version 2 starting

--- main.cpp
+++ main.cpp
@@ -1,11 +1,5 @@
-#include <QtGui/QApplication>
-#include "FMetro.h"
-
-
 int main(int argc, char** argv)
 {
-    QApplication app(argc, argv);
-    FMetro fmetro;
-    fmetro.show();
-    return app.exec();
+
 }
+
