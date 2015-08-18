a0efb79f1cb1b55285e38b678043f9a65c99a952
helene <helene@Foch.home>
2015-08-19 00:38:01 +0200

    - added missing files

--- main.cpp
+++ main.cpp
@@ -1,5 +1,17 @@
+#include <QApplication>
+#include "MainWindow.hpp"
+#include "Controller.hpp"
+
 int main(int argc, char** argv)
 {
+    int retval;
+    QApplication app(argc, argv);
+    MainWindow window;
+    new Controller; // Singleton
 
+    // Show and execute the program
+    window.show();
+    retval = app.exec();
+    delete Controller::get();
+    return retval;
 }
-
