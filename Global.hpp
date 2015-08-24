4b75533ab4eb12cc23a37309b4f39a66c22835f0
helene <helene@Foch.home>
2015-08-24 16:15:19 +0200

    Updated .gitignore

---
+++ Global.hpp
@@ -0,0 +1,11 @@
+#ifndef GLOBAL_HPP
+#define GLOBAL_HPP
+
+#define APPLICATION_NAME_STR "FMetro"
+
+#define GAME_FILE_EXTENSION "fmg"
+#define GAME_FILE_FILTER QString(tr("FMetro game (*.%1)")).arg(GAME_FILE_EXTENSION)
+#define GAME_FILE_SIGNATURE "FMetro2 game file"
+#define GAME_FILE_VERSION 1
+
+#endif // GLOBAL_HPP
